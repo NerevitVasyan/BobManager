c161dfb47d184bf16ae53a10375d7d41462df875
purple <purplespace30@gmail.com>
2019-10-22 17:54:10 +0300

    Created Dto Models

---
+++ BobManager.API/BobManager.Dto/DtoModels/CalendarEventDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BobManager.Dto.DtoModels
+{
+    public class CalendarEventDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public string Color { get; set; }
+        public int? GroupId { get; set; }
+        public int UserId { get; set; }
+    }
+}
