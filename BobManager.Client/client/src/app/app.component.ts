f1ddb865fcb2fde6724ff6d486be33742a82acfc
Smirnov Andrii <Smirnov Andrii@A12PC6>
2019-10-23 17:25:36 +0300

    Resolved comments

--- BobManager.Client/client/src/app/app.component.ts
+++ BobManager.Client/client/src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import {FooterComponent} from './footer/footer.component'
-import {HomeComponent} from './home/home.component'
-import {HeaderComponent} from './header/header.component'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
